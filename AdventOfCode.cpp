14a1ae2d08c9ed0f30453760161e836e89bcab25
Stuart Milne <stukmilne@gmail.com>
2024-12-03 21:16:19 +0000

    Prettyfying with title and print helper for days.

--- AdventOfCode.cpp
+++ AdventOfCode.cpp
@@ -5,8 +5,15 @@
 
 #include <print>
 
+void startDay(const int inDayNum)
+{
+    std::println();;
+    std::println("=== DAY {} ===", inDayNum);
+}
+
 int main()
 {
+    std::println("stumilne's Advent of Code 2024");
     {
         // Day 1
         const auto input = stu::readFile("day1.txt");
@@ -14,26 +21,24 @@ int main()
         const auto listDistance = day1::getDistanceBetweenLocationIDLists(locationListA, locationListB);
         const auto similarity = day1::getSimilarity(locationListA, locationListB);
 
-        std::println("=== DAY 1 ===");
+        startDay(1);
         std::println("The distance between location lists is: {}", listDistance);
         std::println("The Similarity of the lists is: {}", similarity);
     }
 
-    std::println();
     {
         const auto input = stu::readFile("day2.txt");
         const auto numSafeReports = day2::countSafeReports(input.c_str());
 
-        std::println("=== DAY 2 ===");
+        startDay(2);
         std::println("The number of safe reports is: {}", numSafeReports);
     }
-
-    std::println();
+    
     {
         const auto input = stu::readFile("day3.txt");
         const auto sumOfMuls = day3::parseMulInput(input);
 
-        std::println("=== DAY 3 ===");
+        startDay(3);
         std::println("The sum of mul expressions is: {}", sumOfMuls);
     }
 
